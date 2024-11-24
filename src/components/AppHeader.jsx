7e55b7c697734d8fdca8cbd0830edd5b5dfa0996
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-24 22:18:05 +0100

    fix container

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,7 @@
 export default function AppHeader() {
 	return (
 		<header>
-			<div className='container my-3'>
+			<div className='container py-3'>
 				<div className='d-flex justify-content-between align-items-center'>
 					<h1 className='text-center'>React form</h1>
 				</div>
