be38ab34d20d8037433cb80a06042f6da2895754
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-24 22:10:55 +0100

    add header and main

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,11 @@
 export default function AppHeader() {
 	return (
-		<div className='container my-3'>
-			<div className='d-flex justify-content-between align-items-center'>
-				<h1 className='text-center'>React form</h1>
+		<header>
+			<div className='container my-3'>
+				<div className='d-flex justify-content-between align-items-center'>
+					<h1 className='text-center'>React form</h1>
+				</div>
 			</div>
-		</div>
+		</header>
 	)
 }
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,14 +4,16 @@ import AppCard from './AppCard'
 export default function AppMain({ addArticle, handleFormField, formData, articles, deleteArticle }) {
 	return (
 		<>
-			<AppAddArticle addArticle={addArticle} handleFormField={handleFormField} formData={formData} />
-			<section className='container my-3'>
-				<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
-					{articles.map((article, index) => (
-						<AppCard key={index} article={article} index={index} deleteArticle={deleteArticle} />
-					))}
-				</div>
-			</section>
+			<main>
+				<AppAddArticle addArticle={addArticle} handleFormField={handleFormField} formData={formData} />
+				<section className='container my-3'>
+					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+						{articles.map((article, index) => (
+							<AppCard key={index} article={article} index={index} deleteArticle={deleteArticle} />
+						))}
+					</div>
+				</section>
+			</main>
 		</>
 	)
 }
